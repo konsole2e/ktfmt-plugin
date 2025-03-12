c2a83814baa908ad321f162bf6dc46d11b844b02
GitHub Action <action@github.com>
2025-03-12 09:05:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/konsole2e/ktfmtplugin/MyBundle.kt
+++ src/main/kotlin/com/github/konsole2e/ktfmtplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.konsole2e.ktfmtplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/konsole2e/ktfmtplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/konsole2e/ktfmtplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.konsole2e.ktfmtplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
